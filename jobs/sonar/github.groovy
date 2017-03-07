b51dd05260f8f78d512c0365d7da05e9d7ac6848
Sergii Marynenko <marynenko@gmail.com>
2017-03-07 14:34:03 +0200

    .

--- jobs/sonar/github.groovy
+++ jobs/sonar/github.groovy
@@ -2,10 +2,10 @@ node('master') {
     properties([
       [$class: "ParametersDefinitionProperty", parameterDefinitions:
         [
-          [$class: "ChoiceParameterDefinition", name: "PLATFORM_NAME", choices: "linux-x86_64\nlinux-ppc64le\nandroid-arm\nandroid-x86\nlinux-x86", description: "Build project on architecture"],
-          [$class: "LabelParameterDefinition", name: "DOCKER_NODE", defaultValue: "jenkins-slave-cuda", description: "Correct parameters:\nFor x86_64-jenkins-slave-cuda,amd64\nfor PowerPC - ppc,power8"],
-          [$class: "BooleanParameterDefinition", name: "SONAR", defaultValue: false, description: "Select to check code with SonarQube"],
-          [$class: "StringParameterDefinition", name: "GIT_BRANCHNAME", defaultValue: "master", description: "Default Git branch value"],
+          // [$class: "ChoiceParameterDefinition", name: "PLATFORM_NAME", choices: "linux-x86_64\nlinux-ppc64le\nandroid-arm\nandroid-x86\nlinux-x86", description: "Build project on architecture"],
+          // [$class: "LabelParameterDefinition", name: "DOCKER_NODE", defaultValue: "ubuntu4sonar", description: "Correct parameters:\nFor x86_64-jenkins-slave-cuda,amd64\nfor PowerPC - ppc,power8"],
+          // [$class: "BooleanParameterDefinition", name: "SONAR", defaultValue: true, description: "Select to check code with SonarQube"],
+          [$class: "StringParameterDefinition", name: "GIT_BRANCH_NAME", defaultValue: "master", description: "Default Git branch value"],
           [$class: "CredentialsParameterDefinition", name: "GITCREDID", required: false, defaultValue: "github-smaryn-pipelines-id-1", description: "Credentials to access needed repositories"],
           [$class: "StringParameterDefinition", name: "PDIR", defaultValue: "jobs/service", description: "Path to groovy scripts"],
 
@@ -15,7 +15,10 @@ node('master') {
 
     step([$class: 'WsCleanup'])
 
-    // checkout scm
+    checkout scm
+
+    load "${PDIR}/vars.groovy"
+    functions = load "${PDIR}/functions.groovy"
 
     stage('Check sources with SonarQube') {
 
