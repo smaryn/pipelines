1b69ae8be6bac745378097c0e55afeb41635ff47
Sergii Marynenko <marynenko@gmail.com>
2017-03-07 15:20:10 +0200

    Add VERSION

--- jobs/sonar/github.groovy
+++ jobs/sonar/github.groovy
@@ -5,6 +5,7 @@ node('master') {
           // [$class: "ChoiceParameterDefinition", name: "PLATFORM_NAME", choices: "linux-x86_64\nlinux-ppc64le\nandroid-arm\nandroid-x86\nlinux-x86", description: "Build project on architecture"],
           // [$class: "LabelParameterDefinition", name: "DOCKER_NODE", defaultValue: "ubuntu4sonar", description: "Correct parameters:\nFor x86_64-jenkins-slave-cuda,amd64\nfor PowerPC - ppc,power8"],
           // [$class: "BooleanParameterDefinition", name: "SONAR", defaultValue: true, description: "Select to check code with SonarQube"],
+          [$class: "StringParameterDefinition", name: "VERSION", defaultValue: "0.1", description: "Version to pass to SonarQube"],
           [$class: "StringParameterDefinition", name: "ACCOUNT", defaultValue: "smaryn", description: "Default Git account"],
           [$class: "StringParameterDefinition", name: "PROJECT", defaultValue: "pipelines", description: "Default Git project"],
           [$class: "StringParameterDefinition", name: "BRANCH_NAME", defaultValue: "master", description: "Default Git branch value"],
