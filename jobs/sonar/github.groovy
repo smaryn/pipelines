55541613e626176328f17a701a85bd8ad5603909
Sergii Marynenko <marynenko@gmail.com>
2017-03-07 16:28:21 +0200

    Credentials required for ssh access to repository

--- jobs/sonar/github.groovy
+++ jobs/sonar/github.groovy
@@ -1,31 +1,13 @@
 node('master') {
-/*
-    properties([
-      [$class: "ParametersDefinitionProperty", parameterDefinitions:
-        [
-          [buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '3')), pipelineTriggers([])]
-          // [$class: "ChoiceParameterDefinition", name: "PLATFORM_NAME", choices: "linux-x86_64\nlinux-ppc64le\nandroid-arm\nandroid-x86\nlinux-x86", description: "Build project on architecture"],
-          // [$class: "LabelParameterDefinition", name: "DOCKER_NODE", defaultValue: "ubuntu4sonar", description: "Correct parameters:\nFor x86_64-jenkins-slave-cuda,amd64\nfor PowerPC - ppc,power8"],
-          // [$class: "BooleanParameterDefinition", name: "SONAR", defaultValue: true, description: "Select to check code with SonarQube"],
-          [$class: "StringParameterDefinition", name: "VERSION", defaultValue: "0.1", description: "Version to pass to SonarQube"],
-          [$class: "StringParameterDefinition", name: "ACCOUNT", defaultValue: "smaryn", description: "Default Git account"],
-          [$class: "StringParameterDefinition", name: "PROJECT", defaultValue: "pipelines", description: "Default Git project"],
-          [$class: "StringParameterDefinition", name: "BRANCH_NAME", defaultValue: "master", description: "Default Git branch value"],
-          [$class: "CredentialsParameterDefinition", name: "GITCREDID", required: false, defaultValue: "github-smaryn-pipelines-id-1", description: "Credentials to access needed repositories"],
-          [$class: "StringParameterDefinition", name: "PDIR", defaultValue: "jobs/sonar", description: "Path to groovy scripts"],
 
-        ]
-      ]
-    ])
-*/
     properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '3')),
       parameters([string(defaultValue: '0.1', description: 'Version to pass to SonarQube', name: 'VERSION'),
                   string(defaultValue: 'smaryn', description: 'Default Git account', name: 'ACCOUNT'),
                   string(defaultValue: 'pipelines', description: 'Default Git project', name: 'PROJECT'),
                   string(defaultValue: 'master', description: 'Default Git branch value', name: 'BRANCH_NAME'),
-                  [$class: 'CredentialsParameterDefinition', credentialType: 'com.cloudbees.plugins.credentials.common.StandardCredentials', defaultValue: 'github-smaryn-pipelines-id-1', description: 'Credentials to access repositories', name: 'GITCREDID', required: false],
+                  [$class: 'CredentialsParameterDefinition', credentialType: 'com.cloudbees.plugins.credentials.common.StandardCredentials', defaultValue: 'github-smaryn-pipelines-id-1', description: 'Credentials to access repositories', name: 'GITCREDID', required: true],
                   string(defaultValue: 'jobs/sonar', description: 'Path to groovy scripts', name: 'PDIR')]), pipelineTriggers([])])
-                  
+
     stage('Prepare workspace') {
           step([$class: 'WsCleanup'])
 
