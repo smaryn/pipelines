616cdaba215dca3b92d6021fb2661445ecf6ff63
Sergii Marynenko <marynenko@gmail.com>
2017-03-07 17:34:26 +0200

    Use ssh or http 07

--- jobs/sonar/github.groovy
+++ jobs/sonar/github.groovy
@@ -23,9 +23,10 @@ node('master') {
                   string(defaultValue: 'smaryn', description: 'Default Git account', name: 'ACCOUNT'),
                   string(defaultValue: 'pipelines', description: 'Default Git project', name: 'PROJECT'),
                   string(defaultValue: 'master', description: 'Default Git branch value', name: 'BRANCH_NAME'),
+                  choice(choices: ['ssh', 'http'], description: 'Protocol to be used for access to github', name: 'PROTOCOL'),
                   [$class: 'CredentialsParameterDefinition', credentialType: 'com.cloudbees.plugins.credentials.common.StandardCredentials', defaultValue: 'github-smaryn-pipelines-id-1', description: 'Credentials to access repositories', name: 'GITCREDID', required: false],
                   string(defaultValue: 'jobs/sonar', description: 'Path to groovy scripts', name: 'PDIR')]), pipelineTriggers([])])
-                  
+
 
     stage('Prepare workspace') {
           step([$class: 'WsCleanup'])
