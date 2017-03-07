d1ff62f5d42bdb4b14fdb03848bd91df4503b345
Sergii Marynenko <marynenko@gmail.com>
2017-03-07 14:52:45 +0200

    fis errors

--- jobs/sonar/vars.groovy
+++ jobs/sonar/vars.groovy
@@ -3,5 +3,7 @@ env.PROJECT = "pipelines"
 
 env.DOCKER_UBUNTU_SONAR = "zensam/ubuntu4sonar:latest"
 
-dockerParams = "-v ${WORKSPACE}:${WORKSPACE}:rw -v ${JENKINS_M2DIR_AMD64}:/home/jenkins/.m2:rw"
-dockerParams_tmpfs = "-v ${WORKSPACE}:${WORKSPACE}:rw ${JENKINS_M2DIR_AMD64}:/home/jenkins/.m2:rw --tmpfs /tmp:size=3g"
+env.JENKINS_M2DIR = "/var/lib/jenkins/storage/docker_m2"
+
+dockerParams = "-v ${WORKSPACE}:${WORKSPACE}:rw -v ${JENKINS_M2DIR}:/home/jenkins/.m2:rw"
+dockerParams_tmpfs = "-v ${WORKSPACE}:${WORKSPACE}:rw ${JENKINS_M2DIR}:/home/jenkins/.m2:rw --tmpfs /tmp:size=3g"
