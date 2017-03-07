97270cb9a8fa8e5b0118582d53636d9f79bc4140
Sergii Marynenko <marynenko@gmail.com>
2017-03-07 17:29:36 +0200

    Use ssh or http 04

--- jobs/sonar/github.groovy
+++ jobs/sonar/github.groovy
@@ -1,13 +1,5 @@
 node('master') {
-  properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '3')),
-  parameters([string(defaultValue: '0.1', description: 'Version to pass to SonarQube', name: 'VERSION'),
-              string(defaultValue: 'smaryn', description: 'Default Git account', name: 'ACCOUNT'),
-              string(defaultValue: 'pipelines', description: 'Default Git project', name: 'PROJECT'),
-              string(defaultValue: 'master', description: 'Default Git branch value', name: 'BRANCH_NAME'),
-              choice(choices: ['ssh', 'http'], description: 'Protocol to be used for access to github', name: 'PROTOCOL'),
-              [$class: 'CredentialsParameterDefinition', credentialType: 'com.cloudbees.plugins.credentials.common.StandardCredentials', defaultValue: 'github-smaryn-pipelines-id-1', description: 'Credentials to access repositories', name: 'GITCREDID', required: false],
-              string(defaultValue: 'jobs/sonar', description: 'Path to groovy scripts', name: 'PDIR')]),
-              pipelineTriggers([])])
+properties([parameters([string(defaultValue: '0.1', description: 'Version to pass to SonarQube', name: 'VERSION'), string(defaultValue: 'smaryn', description: 'Default Git account', name: 'ACCOUNT'), string(defaultValue: 'pipelines', description: 'Default Git project', name: 'PROJECT'), string(defaultValue: 'master', description: 'Default Git branch value', name: 'BRANCH_NAME'), choice(choices: ['ssh', 'http'], description: 'Protocol to be used for access to github', name: 'PROTOCOL'), [$class: 'CredentialsParameterDefinition', credentialType: 'com.cloudbees.plugins.credentials.common.StandardCredentials', defaultValue: 'github-smaryn-pipelines-id-1', description: 'Credentials to access repositories', name: 'GITCREDID', required: false], string(defaultValue: 'jobs/sonar', description: 'Path to groovy scripts', name: 'PDIR')]), pipelineTriggers([])])
 
     stage('Prepare workspace') {
           step([$class: 'WsCleanup'])
