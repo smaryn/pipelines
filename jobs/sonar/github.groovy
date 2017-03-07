947eb24a19097634ae17bccaf16bd513302d109d
Sergii Marynenko <marynenko@gmail.com>
2017-03-07 14:41:11 +0200

    Set default project

--- jobs/sonar/github.groovy
+++ jobs/sonar/github.groovy
@@ -6,7 +6,7 @@ node('master') {
           // [$class: "LabelParameterDefinition", name: "DOCKER_NODE", defaultValue: "ubuntu4sonar", description: "Correct parameters:\nFor x86_64-jenkins-slave-cuda,amd64\nfor PowerPC - ppc,power8"],
           // [$class: "BooleanParameterDefinition", name: "SONAR", defaultValue: true, description: "Select to check code with SonarQube"],
           [$class: "StringParameterDefinition", name: "ACCOUNT", defaultValue: "smaryn", description: "Default Git account"],
-          [$class: "StringParameterDefinition", name: "PROJECT", defaultValue: "smaryn", description: "Default Git project"],
+          [$class: "StringParameterDefinition", name: "PROJECT", defaultValue: "pipelines", description: "Default Git project"],
           [$class: "StringParameterDefinition", name: "BRANCH_NAME", defaultValue: "master", description: "Default Git branch value"],
           [$class: "CredentialsParameterDefinition", name: "GITCREDID", required: false, defaultValue: "github-smaryn-pipelines-id-1", description: "Credentials to access needed repositories"],
           [$class: "StringParameterDefinition", name: "PDIR", defaultValue: "jobs/sonar", description: "Path to groovy scripts"],
